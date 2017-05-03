9ac4107f5dcefcffe06c81c3e240ad292a1bd0ca
KOOLANOO\letie <ltdat>
2017-05-03 14:31:11 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data
+{
+    public class TeduShopDbContext:DbContext
+    {
+        public TeduShopDbContext() : base("TeduShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+
+        }
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Menu> Menus { set; get; }
+        public DbSet<MenuGroup> MenuGroups { set; get; }
+        public DbSet<Footer> Footers { set; get; }
+        public DbSet<Footer> Footers { set; get; }
+
+    }
+}
