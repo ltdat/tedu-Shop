d0a329ca953dc0a916ae0020fc215153a6299823
KOOLANOO\letie <ltdat>
2017-05-04 11:29:48 +0700

    fixbug for repository and add post services

--- TeduShop.Data/Repositories/ProductCategoryRepository.cs
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -9,7 +9,7 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IProductCategoryRepository
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
     {
         IEnumerable<ProductCategory> GetByAlias(string alias);
     }
