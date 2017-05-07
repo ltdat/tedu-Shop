f9344e2095b5920fd0431c6aa3dc909b2a4073cd
KOOLANOO\letie <ltdat>
2017-05-07 23:48:07 +0700

    scope & rootScope

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -1,10 +1,23 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 var myApp = angular.module('myModule', []);
-myApp.controller("myController",myController);
+//myApp.controller("myController",myController);
 
-myController.$inject=['$scope'];
+//myController.$inject=['$scope'];
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
 
 //fucntion
-function myController($scope) {
-    $scope.message="This is my message from Controller";
+//function studentController($rootScope, $scope) {
+//    $rootScope.message="This is my message from Controller";
+//}
+
+function schoolController($scope) {
+    $scope.message = "Announcement from school";
+}
+function studentController($scope) {
+    //$scope.message = "This is my message from student";
+}
+function teacherController($scope) {
+    //$scope.message = "This is my message from teacher";
 }
