62942097bd1d049bbbf2a7dd2fdec38a5998c76c
KOOLANOO\letie <ltdat>
2017-05-09 21:31:50 +0700

    route implement for apps

---
+++ TeduShop.Web/app/components/products/products.module.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="F:\Git\TeduShop\TeduShop.Web\Assets/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('tedushop.products', ['tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouter) {
+        $stateProvider.state('products', {
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productController"
+        }).state('product_add', {
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
