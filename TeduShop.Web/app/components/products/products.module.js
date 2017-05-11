2300fa659e8d2613864cd78d41f2ea497bc0e778
KOOLANOO\letie <ltdat>
2017-05-12 06:39:50 +0700

    use pagerdirective to paging

--- TeduShop.Web/app/components/products/products.module.js
+++ TeduShop.Web/app/components/products/products.module.js
@@ -8,7 +8,7 @@
         $stateProvider.state('products', {
             url: "/products",
             templateUrl: "/app/components/products/productListView.html",
-            controller: "productListController",
+            controller: "productListController"
         }).state('product_add', {
             url: "/product_add",
             templateUrl: "/app/components/products/productAddView.html",
