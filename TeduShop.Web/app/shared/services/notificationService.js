5a6a87259b58c7f75fb1e2d1d65535747b250d5a
KOOLANOO\letie <ltdat>
2017-05-12 23:56:28 +0700

    add notificationService

---
+++ TeduShop.Web/app/shared/services/notificationService.js
@@ -0,0 +1,41 @@
+﻿(function (app) {
+    app.factory('notificationService', notificationService);
+    function notificationService() {
+        toastr.options = {
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "fadeIn": 300,
+            "fadeOut": 1000,
+            "timeOut": 3000,
+            "extendedTimeOut": 1000
+        };
+        
+        function success(message) {
+            toastr.success(message);
+        }
+        function error(error) {
+            if (Array.isArray(error)) {
+                error.each(function (err) {
+                    toastr.error(err);
+                });
+            }
+            else {
+                toastr.error(error);
+            }
+        }
+        function warning(message) {
+            toastr.warning(message);
+        }
+        function info(message) {
+            toastr.info(message);
+        }
+
+        return {
+            success: success,
+            error: error,
+            warning: warning,
+            info: info
+        }
+    }
+})(angular.module('tedushop.common'));
