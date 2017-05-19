8ce93e77fd7bef10ba63ecdbcfb3ecdab189041b
KOOLANOO\letie <letiendat.ltd@outlook.com>
2017-05-19 00:53:14 -0700

    delete multi record

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using TeduShop.Model.Models;
 using TeduShop.Service;
 using TeduShop.Web.Infrastructure.Core;
@@ -161,6 +162,37 @@ namespace TeduShop.Web.Api
                     _productCategoryService.Save();
                     var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(oldProductCatgory);
                     response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                    return response;
+                }
+                return response;
+            });
+        }
+
+
+        [Route("deletemulti")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage DeleteMulti(HttpRequestMessage request, string checkedProductCategories)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listProductCategory = new JavaScriptSerializer().Deserialize<List<int>>(checkedProductCategories);
+                    foreach(var item in listProductCategory)
+                    {
+                        _productCategoryService.Delete(item);
+
+                    }
+                    
+                    _productCategoryService.Save();
+                   
+                    response = request.CreateResponse(HttpStatusCode.OK, listProductCategory.Count);
                     return response;
                 }
                 return response;
--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService','notificationService','$ngBootbox'];
+    productCategoryListController.$inject = ['$scope', 'apiService','notificationService','$ngBootbox','$filter'];
 
-    function productCategoryListController($scope, apiService, notificationService, $ngBootbox) {
+    function productCategoryListController($scope, apiService, notificationService, $ngBootbox,$filter) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -11,6 +11,56 @@
         $scope.keyword = "";
         $scope.search = search;
         $scope.deleteProductCategory = deleteProductCategory;
+        $scope.selectAll = selectAll;
+
+        $scope.deleteMultiple = deleteMultiple;
+
+        function deleteMultiple() {
+            var listID = [];
+            $.each($scope.selected, function (i, item) {
+                listID.push(item.ID);
+            });
+            var config={
+                params:{
+                    checkedProductCategories:JSON.stringify(listID)
+                }
+            }
+            apiService.del('api/productcategory/deletemulti', config, function (result) {
+                notificationService.displaySuccess('Xóa thành công ' + result.data + ' bản ghi.');
+                search();
+
+            }, function (error) {
+                notificationService.displayError('Xóa không thành công' + ' bản ghi.');
+            });
+        }
+        
+        $scope.isAll = false;
+        function selectAll() {
+            if ($scope.isAll === false) {
+                angular.forEach($scope.productCategories, function (item) {
+                    item.checked = true;
+                });
+                $scope.isAll = true;
+            } else {
+                angular.forEach($scope.productCategories, function (item) {
+                    item.checked = false;
+                });
+                $scope.isAll = false;
+            }
+        }
+
+
+        $scope.$watch("productCategories", function (n, o) {
+            var checked = $filter("filter")(n, { checked: true });
+            if (checked.length) {
+                $scope.selected = checked;
+                $('#btnDelete').removeAttr('disabled');
+
+            }
+            else {
+                $('#btnDelete').attr('disabled', 'disabled');
+            }
+        },true);
         function deleteProductCategory(id) {
             $ngBootbox.confirm('Bạn chắc chắn muốn xóa mục này?').then(function () {
                 var config = {
